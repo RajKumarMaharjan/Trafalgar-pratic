0c641d091be5972ea2b9856efdb455ef640c1eb5
Raj kumar Maharjan <raazmaharjan69@gmail.com>
2023-05-15 06:45:47 +0545

    devlop fontend web desing using bootstrap, scss & javaScripts

--- README.md
+++
@@ -1,4 +0,0 @@
-# yipl-frontend-challenge
-# main index.html in pubic file
-# develop with scss
-# if code error please use scss watching 
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,4 @@
 let mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-        myBtn.style.display = "block";
-    } else {
-        myBtn.style.display = "none";
-    }
-  };
-
   function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
